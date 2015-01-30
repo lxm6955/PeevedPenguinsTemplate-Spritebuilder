04e9bb77943af8787a6c12b4976a8b1fe78fcfac
Himo <Himo@lius-MBP.sv.cmu.local>
2015-01-29 18:37:41 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Himo on 15/1/29.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
