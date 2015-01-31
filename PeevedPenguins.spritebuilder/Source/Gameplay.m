9b38b26f0ba41f1405aae5b2e9724478b856ceb5
Himo <Himo@lius-MBP.sv.cmu.local>
2015-01-30 18:03:42 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -48,13 +48,9 @@
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
     // ensure followed object is in visible are when starting
-   // self.position = ccp(0, 0);
-   // CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-   // [self runAction:follow];
-    // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [_contentNode runAction:follow];
+    [self runAction:follow];
 }
 
 @end
